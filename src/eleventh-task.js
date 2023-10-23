c302c3bc2d00e12d87ee58a87a1eb4b299692f01
luka0204 <luka9609@gmail.com>
2023-10-23 12:21:33 +0300

    eleventh-task

---
+++ src/eleventh-task.js
@@ -0,0 +1,9 @@
+const hours = (3665 - 65) / 60 / 60 ;
+const minutes = (3665 - 65) / 60 / 60;
+const seconds = 3665 - ((60 * 60) + 60);
+
+
+
+
+
+console.log(hours + " ч", minutes + " мин", seconds + " сек");
